2cddd6565eab27befc533c9893ffbd662618139d
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-05-13 16:10:18 +0430

    added the AI

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function poemGenerator(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "به روز مرگ چو تابوت من روان باشد",
+    strings: response.data.answer,
     autoStart: true,
     delay: "100",
     cursor: "",
   });
+}
+
+function poemGenerator(event) {
+  event.preventDefault();
+  let userInstruction = document.querySelector("#user-instruction");
+  let apiKey = "8tc45bb70811ad02c7716f84fe4ocbe3";
+  let prompt = `Write a Persian poem about ${userInstruction.value}`;
+  let context =
+    "You are a romantic poeom generator AI an love short poems. Your maission is to generate a four line Dari poem from Afgan and Persian poets and separate each line with a <br />.  Make sure to follow the user instruction. please sign it with the <strong>poet Name</strong>  ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let peomElement = document.querySelector("#poem-generator");
